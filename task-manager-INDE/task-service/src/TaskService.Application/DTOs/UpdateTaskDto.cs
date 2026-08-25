d68a1aebe5a9defe9d450b09812e4ec79bf5b997
egomez-2021272 <egomez-2021272@kinal.edu.gt>
2026-08-25 14:57:48 -0600

    feat(TaskService.Application): Agregar Dtos, validaciones y archivos necesarios para el mapeo y procesamiento de las reglas del negocio

---
+++ task-manager-INDE/task-service/src/TaskService.Application/DTOs/UpdateTaskDto.cs
@@ -0,0 +1,8 @@
+namespace TaskService.Application.DTOs;
+
+public class UpdateTaskDto
+{
+    public string Title { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public string Status { get; set; } = string.Empty; // los 3 que existen en el programa, en progreso, hecho, pendiente
+}
