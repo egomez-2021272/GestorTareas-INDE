aaf6656dc6c57ade16cd1066ede08e922e4549fa
lcastro-2021276 <lcastro-2021276@kinal.edu.gt>
2026-08-25 12:13:02 -0600

    feat(task-service): implementar entidades de dominio y persistencia junto con el archivo de configuracion docker-compose

---
+++ task-manager-INDE/task-service/src/TaskService.Domain/Entities/TaskItem.cs
@@ -0,0 +1,17 @@
+using TaskService.Domain.Enums;
+
+namespace TaskService.Domain.Entities;
+
+public class TaskItem
+{
+    public Guid Id { get; set; } = Guid.NewGuid();
+    public string Title { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public Enums.TaskStatus Status { get; set; } = Enums.TaskStatus.ToDo; 
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    public DateTime? UpdatedAt { get; set; }
+
+   
+    public ICollection<Tag> Tags { get; set; } = new List<Tag>();
+}
+//Representa una tarjeta o la tarea en un tablero
